c823beb8fbb2f04fb46647bfce0f32ac868cd613
caemanswartz <caemanswartz@gmail.com>
2022-07-18 09:01:14 -0700

    removed autotiles and script in favor for tilemap of single pieces

--- shared/Scenes/World/World.cs
+++ shared/Scenes/World/World.cs
@@ -6,58 +6,58 @@ using SharpScape.Game.Services;
 
 public class World : Node2D
 {
-    [Signal] delegate void AvatarSpawned(GameAvatar who);
-    [Signal] delegate void WorldLoadingComplete();
+	[Signal] delegate void AvatarSpawned(GameAvatar who);
+	[Signal] delegate void WorldLoadingComplete();
 
-    private PackedScene _avatarScene;
+	private PackedScene _avatarScene;
 
-    public override void _Ready()
-    {
-        GD.Randomize();
+	public override void _Ready()
+	{
+		GD.Randomize();
 
-        _avatarScene = GD.Load<PackedScene>("res://shared/Scenes/GameAvatar/GameAvatar.tscn");
+		_avatarScene = GD.Load<PackedScene>("res://shared/Scenes/GameAvatar/GameAvatar.tscn");
 
-        var _networkService = this.GetSingleton<NetworkServiceNode>();
-        Connect(nameof(WorldLoadingComplete), _networkService, "_OnWorldLoadingComplete", flags: (uint)ConnectFlags.Oneshot);
-        Connect(nameof(AvatarSpawned), _networkService, "_OnWorldAvatarSpawned");
+		var _networkService = this.GetSingleton<NetworkServiceNode>();
+		Connect(nameof(WorldLoadingComplete), _networkService, "_OnWorldLoadingComplete", flags: (uint)ConnectFlags.Oneshot);
+		Connect(nameof(AvatarSpawned), _networkService, "_OnWorldAvatarSpawned");
 
-        GetTree().Connect("idle_frame", this,
-            method: "emit_signal",
-            binds: new Godot.Collections.Array { nameof(WorldLoadingComplete) },
-            flags: (uint)ConnectFlags.Oneshot);
-    }
+		GetTree().Connect("idle_frame", this,
+			method: "emit_signal",
+			binds: new Godot.Collections.Array { nameof(WorldLoadingComplete) },
+			flags: (uint)ConnectFlags.Oneshot);
+	}
 
-    public override void _ExitTree()
-    {
-        this.GetSingleton<NetworkServiceNode>().QueueFree();
-    }
+	public override void _ExitTree()
+	{
+		this.GetSingleton<NetworkServiceNode>().QueueFree();
+	}
 
-    public GameAvatar SpawnGameAvatar(string playerInfo)
-    {
-        var player = Utils.FromJson<PlayerInfo>(playerInfo);
-        var existingPlayer = GetTree().GetNodesInGroup("Players").OfType<GameAvatar>().FirstOrDefault(p => p.UserId == player.UserInfo.Id);
-        if (IsInstanceValid(existingPlayer))
-        {
-            existingPlayer.QueueFree();
-        }
+	public GameAvatar SpawnGameAvatar(string playerInfo)
+	{
+		var player = Utils.FromJson<PlayerInfo>(playerInfo);
+		var existingPlayer = GetTree().GetNodesInGroup("Players").OfType<GameAvatar>().FirstOrDefault(p => p.UserId == player.UserInfo.Id);
+		if (IsInstanceValid(existingPlayer))
+		{
+			existingPlayer.QueueFree();
+		}
 
-        var avatar = _avatarScene.Instance() as GameAvatar;
-        AddChild(avatar);
-        avatar.UserId = player.UserInfo.Id;
-        avatar.Username = player.UserInfo.Username;
-        avatar.GlobalPosition = new Vector2(player.GlobalPositionX, player.GlobalPositionY);
-        avatar.SetAnimatedSpriteFrames(player.SpriteName);
+		var avatar = _avatarScene.Instance() as GameAvatar;
+		AddChild(avatar);
+		avatar.UserId = player.UserInfo.Id;
+		avatar.Username = player.UserInfo.Username;
+		avatar.GlobalPosition = new Vector2(player.GlobalPositionX, player.GlobalPositionY);
+		avatar.SetAnimatedSpriteFrames(player.SpriteName);
 
-        EmitSignal(nameof(AvatarSpawned), avatar);
-        return avatar;
-    }
-    public void DespawnGameAvatar(string playerInfo)
-    {
-        var player = Utils.FromJson<PlayerInfo>(playerInfo);
-        var existingPlayer = GetTree().GetNodesInGroup("Players").OfType<GameAvatar>().FirstOrDefault(p => p.UserId == player.UserInfo.Id);
-        if (IsInstanceValid(existingPlayer))
-        {
-            existingPlayer.QueueFree();
-        }
-    }
+		EmitSignal(nameof(AvatarSpawned), avatar);
+		return avatar;
+	}
+	public void DespawnGameAvatar(string playerInfo)
+	{
+		var player = Utils.FromJson<PlayerInfo>(playerInfo);
+		var existingPlayer = GetTree().GetNodesInGroup("Players").OfType<GameAvatar>().FirstOrDefault(p => p.UserId == player.UserInfo.Id);
+		if (IsInstanceValid(existingPlayer))
+		{
+			existingPlayer.QueueFree();
+		}
+	}
 }
